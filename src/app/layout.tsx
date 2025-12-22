a74aed92a81fc63652630d1935ae7ae84b712b1b
geregere <flare.g255@gmail.com>
2025-12-22 17:56:13 +0900

    GTMの導入

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, Inter, Teko, Orbitron } from "next/font/google";
+import { GoogleTagManager } from '@next/third-parties/google'
 import "./globals.css";
 
 const geistSans = Geist({
@@ -41,6 +42,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ja" suppressHydrationWarning>
+      <GoogleTagManager gtmId="GTM-TCMHWF2W" />
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${inter.variable} ${teko.variable} ${orbitron.variable} antialiased`}
       >
