c7f4cca6676977240d0b16b6f795595a82cb8a0d
geregere <flare.g255@gmail.com>
2025-12-23 11:27:18 +0900

    背景をレイアウトに移動しようとしたが、やはりトップでいいことが分かったので戻す。

---
+++ src/components/TopMainScene.tsx
@@ -0,0 +1,55 @@
+"use client";
+
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+import { Bloom, EffectComposer } from "@react-three/postprocessing";
+import { useState, useEffect } from "react";
+import ParticlesDisplay from "./ParticlesDisplay";
+import WavingLines from "./WavingLines";
+
+export default function TopMainScene() {
+    const [isClient, setIsClient] = useState(false);
+    useEffect(() => { setIsClient(true); }, []);
+
+
+    if (!isClient) {
+        return <div className="main-scene-placeholder" />;
+    }
+
+    return (
+        <div className="main-scene-container">
+            <Canvas
+                shadows
+                camera={{ position: [0, 2, 6], fov: 75 }}
+                gl={{ antialias: false }}
+            >
+                <color attach="background" args={['#101010']} />
+                <fog attach="fog" args={['#101010', 200, 2000]} />
+
+                <ambientLight intensity={0.2} />
+
+                <pointLight position={[10, 10, 10]} intensity={2.0} castShadow color="#00ffff" />
+                <pointLight position={[-10, -5, -5]} intensity={1.0} color="#ff00ff" />
+
+                <OrbitControls
+                    makeDefault
+                    minDistance={2}
+                    maxDistance={12}
+                    enableDamping
+                />
+
+                <WavingLines />
+                <ParticlesDisplay />
+
+                <EffectComposer>
+                    <Bloom
+                        intensity={2.5}
+                        luminanceThreshold={0}
+                        mipmapBlur
+                        radius={0.7}
+                    />
+                </EffectComposer>
+            </Canvas>
+        </div>
+    );
+}
